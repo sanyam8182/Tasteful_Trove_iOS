59f3cf6ebf6379789d5a8c6c0cbd547f18d3d75f
sanyamkoul <sanyam8182@gmail.com>
2023-05-30 09:44:32 +0530

    HomeScreen View

---
+++ Tasteful Trove/HomeScreen/Controller/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  Tasteful Trove
+//
+//  Created by Sanyam Koul on 30/05/23.
+//
+
+import Foundation
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+    }
+    
+}
